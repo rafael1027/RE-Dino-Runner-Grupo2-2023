9b6e89cec26892000158c66b42bb4963b3dd049d
rafael1027 <rhoyos92@gmail.com>
2023-05-02 19:14:29 -0400

    tarea 2
    implementar las imagenes LARGE_CAPTUS

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,9 +1,15 @@
 import random
 from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
 
 
 class  Cactus(Obstacle):
     def __init__(self,image):
         self.type = random.randint(0,2)
         super().__init__(image, self.type)
-        self.rect.y = 325
+        if image == LARGE_CACTUS:
+            self.rect.y = 300
+        else:
+            self.rect.y = 325
+            
+        
